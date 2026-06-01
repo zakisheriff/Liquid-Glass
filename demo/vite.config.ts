ca8eb34a98ab6f2df15570e58fc78a8b52a1dea4
Zaki Sheriff <zakisheriff57@gmail.com>
2026-06-01 14:24:13 +0530

    Fix sitemap to include only site URLs

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -22,7 +22,7 @@ const seoAssets = () => ({
     this.emitFile({
       type: 'asset',
       fileName: 'sitemap.xml',
-      source: `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n  <url>\n    <loc>${siteUrl}/</loc>\n    <changefreq>weekly</changefreq>\n    <priority>1.0</priority>\n  </url>\n  <url>\n    <loc>https://www.npmjs.com/package/@zakisheriff/liquid-glass</loc>\n    <changefreq>weekly</changefreq>\n    <priority>0.9</priority>\n  </url>\n  <url>\n    <loc>https://github.com/zakisheriff/Liquid-Glass</loc>\n    <changefreq>weekly</changefreq>\n    <priority>0.8</priority>\n  </url>\n</urlset>\n`,
+      source: `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n  <url>\n    <loc>${siteUrl}/</loc>\n    <changefreq>weekly</changefreq>\n    <priority>1.0</priority>\n  </url>\n</urlset>\n`,
     });
 
     this.emitFile({
