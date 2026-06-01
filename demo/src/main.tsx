330226f9dc19b57d2d4d658dbefde3f39e5fb891
Zaki Sheriff <zakisheriff57@gmail.com>
2026-06-01 15:40:21 +0530

    Add Vercel Analytics to demo

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import '@zakisheriff/liquid-glass/styles.css';
 import './index.css';
@@ -7,5 +8,6 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 );
